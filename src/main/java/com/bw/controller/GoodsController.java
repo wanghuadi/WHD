b4e23869c21b3c37afac36db93b3e9ca2fe6b329
whd <wanghd@5ha.com>
2019-09-04 16:05:21 +0800

    dsadsadsa

--- src/main/java/com/bw/controller/GoodsController.java
+++ src/main/java/com/bw/controller/GoodsController.java
@@ -23,7 +23,7 @@ public class GoodsController {
 	public String queryGoodsAll(Model model){
 		List<Goods> list = service.queryGoodsAll();
 		model.addAttribute("list", list); 
-		System.out.println("sssssssssss"); //xxx
+		System.out.println("sssssssddddssss"); //xxx
 		return "index";
 	}
 	
